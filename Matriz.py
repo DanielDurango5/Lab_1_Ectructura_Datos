5d288a497300b62f26cab8d11873ebc4eec5b464
Daniel Durango <daniel.durango@udea.edu.co>
2026-03-02 20:37:40 -0500

    Code for the first laboratory of Extructura de Datos

---
+++ Matriz.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+filas = 100_000
+columnas = 100_000
+nombre_archivo = "matriz_bits.bin"
+
+with open(nombre_archivo, "wb") as f:
+    for _ in range(filas):
+        fila = np.random.randint(0, 2, size=columnas, dtype=np.uint8)
+        fila_comprimida = np.packbits(fila)
+        f.write(fila_comprimida)
+
+print("Archivo creado correctamente (modo bits).")
